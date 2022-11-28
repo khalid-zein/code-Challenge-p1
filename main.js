dd6e89c17105310b5be5edba3f7285c27aab5999
khalid-zein <zeinkhalid004@gmail.com>
2022-11-28 15:08:36 +0300

    completed challenge 2

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function formReset(){
 btn.addEventListener("click", function (event){
     event.preventDefault();
     var result = document.getElementById("result").value;
-    const output = document.getElementById("output");
+    const output = document.getElementById("output"); 
 
     console.log(result);
     if (result > 79){
@@ -17,7 +17,7 @@ btn.addEventListener("click", function (event){
         output.innerHTML = "B";
     }else if(result <= 59 && result >= 49){
         output.innerHTML = "C";
-    }else if(result <= 49 && result >= 40){
+    }else if(result <= 49 && result >= 40){ 
         output.innerHTML = "D";
     }else if(result < 40){
         output.innerHTML = "E";
@@ -26,4 +26,35 @@ btn.addEventListener("click", function (event){
 
 
     formReset();
+});
+
+// Code Challenge 2
+const speed = document.getElementById("speed");
+
+function formReseting(){
+    document.getElementById("myForm").reset();
+}
+
+
+speed.addEventListener("click", function (e){
+    e.preventDefault();
+    var speedDriven = document.getElementById("speedDriven").value;
+    const speedLimit = 70;
+    const detector = document.getElementById("detector");
+
+    console.log(speedDriven);
+    console.log(speedLimit);
+
+    let demerit = (speedDriven -speedLimit)/5;
+    console.log(demerit);
+    if(speedDriven < 70){
+        detector.innerHTML = "OK";
+    }else if(speedDriven > speedLimit && demerit <= 12){
+        detector.innerHTML = demerit;
+    }else if(demerit > 12){
+        detector.innerHTML = "License Suspended";
+    }
+
+
+    formReseting();
 });
